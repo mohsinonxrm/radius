08f43ab48d26d2f6aabc2e4a5be950ca69c38668
mishrapratikshya <pratmishra@microsoft.com>
2022-09-14 13:31:49 -0700

    Schema update to add recipe to rediscache connector seagger

--- pkg/connectorrp/api/v20220315privatepreview/zz_generated_models_serde.go
+++ pkg/connectorrp/api/v20220315privatepreview/zz_generated_models_serde.go
@@ -1755,6 +1755,7 @@ func (r RedisCacheProperties) MarshalJSON() ([]byte, error) {
 	populate(objectMap, "host", r.Host)
 	populate(objectMap, "port", r.Port)
 	populate(objectMap, "provisioningState", r.ProvisioningState)
+	populate(objectMap, "recipe", r.Recipe)
 	populate(objectMap, "resource", r.Resource)
 	populate(objectMap, "secrets", r.Secrets)
 	populate(objectMap, "status", r.Status)
@@ -1786,6 +1787,9 @@ func (r *RedisCacheProperties) UnmarshalJSON(data []byte) error {
 		case "provisioningState":
 				err = unpopulate(val, "ProvisioningState", &r.ProvisioningState)
 				delete(rawMsg, key)
+		case "recipe":
+				err = unpopulate(val, "Recipe", &r.Recipe)
+				delete(rawMsg, key)
 		case "resource":
 				err = unpopulate(val, "Resource", &r.Resource)
 				delete(rawMsg, key)
@@ -1797,6 +1801,37 @@ func (r *RedisCacheProperties) UnmarshalJSON(data []byte) error {
 				delete(rawMsg, key)
 		case "username":
 				err = unpopulate(val, "Username", &r.Username)
+				delete(rawMsg, key)
+		}
+		if err != nil {
+			return fmt.Errorf("unmarshalling type %T: %v", r, err)
+		}
+	}
+	return nil
+}
+
+// MarshalJSON implements the json.Marshaller interface for type RedisCacheRecipe.
+func (r RedisCacheRecipe) MarshalJSON() ([]byte, error) {
+	objectMap := make(map[string]interface{})
+	populate(objectMap, "name", r.Name)
+	populate(objectMap, "param", r.Param)
+	return json.Marshal(objectMap)
+}
+
+// UnmarshalJSON implements the json.Unmarshaller interface for type RedisCacheRecipe.
+func (r *RedisCacheRecipe) UnmarshalJSON(data []byte) error {
+	var rawMsg map[string]json.RawMessage
+	if err := json.Unmarshal(data, &rawMsg); err != nil {
+		return fmt.Errorf("unmarshalling type %T: %v", r, err)
+	}
+	for key, val := range rawMsg {
+		var err error
+		switch key {
+		case "name":
+				err = unpopulate(val, "Name", &r.Name)
+				delete(rawMsg, key)
+		case "param":
+				err = unpopulate(val, "Param", &r.Param)
 				delete(rawMsg, key)
 		}
 		if err != nil {
@@ -1865,6 +1900,7 @@ func (r RedisCacheResponseProperties) MarshalJSON() ([]byte, error) {
 	populate(objectMap, "host", r.Host)
 	populate(objectMap, "port", r.Port)
 	populate(objectMap, "provisioningState", r.ProvisioningState)
+	populate(objectMap, "recipe", r.Recipe)
 	populate(objectMap, "resource", r.Resource)
 	populate(objectMap, "status", r.Status)
 	populate(objectMap, "username", r.Username)
@@ -1894,6 +1930,9 @@ func (r *RedisCacheResponseProperties) UnmarshalJSON(data []byte) error {
 				delete(rawMsg, key)
 		case "provisioningState":
 				err = unpopulate(val, "ProvisioningState", &r.ProvisioningState)
+				delete(rawMsg, key)
+		case "recipe":
+				err = unpopulate(val, "Recipe", &r.Recipe)
 				delete(rawMsg, key)
 		case "resource":
 				err = unpopulate(val, "Resource", &r.Resource)
